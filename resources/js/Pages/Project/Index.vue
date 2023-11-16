87c5648282afd3c156264c92dac50bb785afc6e7
syuumu <syuumu200@gmail.com>
2023-11-16 11:28:36 +0900

    fix

--- resources/js/Pages/Project/Index.vue
+++ resources/js/Pages/Project/Index.vue
@@ -3,7 +3,7 @@
     <div class="grid grid-cols-1 divide-y gap-3">
       <Link as="article" class="cursor-pointer hover:opacity-70" :href="$route('projects.show', project)" v-for="(project, key) in projects"
         :key="key">
-      <div class="flex justify-between items-center">
+      <div class="flex justify-between flex-wrap items-center">
         <h1 class="font-bold">{{ project.name }}</h1>
         <span>{{ project.user.username }}</span>
       </div>
