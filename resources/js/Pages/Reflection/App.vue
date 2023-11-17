67c83bd6b386d768c21f80829fc8264ae0dd2712
syuumu <syuumu200@gmail.com>
2023-11-17 23:38:31 +0900

    update

--- resources/js/Pages/Reflection/App.vue
+++ resources/js/Pages/Reflection/App.vue
@@ -1,20 +1,22 @@
 <template>
   <Back class="absolute top-1 left-1" :href="$route('projects.show', project)" />
-  <div class="grid md:grid-cols-2">
-    <svg class="invisible md:visible h-full w-full" id="graph"></svg>
-    <div v-if="suggestion" class="whitespace-pre-line md:overflow-y-auto h-screen">{{ suggestion }}</div>
-    <div class="md:mt-20 text-center" v-else>
-      <h1>意見の振り返り</h1>
-      <p>ChatGPTが意見を整理し，考察を行います。文章の生成に時間がかかる場合があります。2分をすぎても出力されない場合は管理者に報告してください。</p>
-      <button class="btn mt-3" @click="finish()" :disabled="isLoading">意見を振り返る</button>
+  <div class="grid md:grid-cols-4">
+    <svg id="graph" class="md:col-span-3 hidden md:block md:h-screen md:w-screen"></svg>
+    <div class="md:col-span-1 md:bg-gray-50 md:px-5">
+      <div v-if="suggestion" class="whitespace-pre-line md:overflow-y-auto h-screen">{{ suggestion }}</div>
+      <div class="mt-10 md:mt-20 text-center" v-else>
+        <h1>意見の振り返り</h1>
+        <p>ChatGPTが意見を整理し，考察を行います。文章の生成に時間がかかる場合があります。2分をすぎても出力されない場合は管理者に報告してください。</p>
+        <button class="btn mt-3" @click="finish()" :disabled="isLoading">意見を振り返る</button>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import { graphviz } from 'd3-graphviz'
+import { graphviz } from 'd3-graphviz';
 import Back from "@/Assets/Back.vue";
-import { router } from '@inertiajs/vue3'
+import { router } from '@inertiajs/vue3';
 
 
 export default {
@@ -39,21 +41,23 @@ export default {
     this.render()
   },
   methods: {
-    render: function () {
+    render: async function () {
       let dot = `digraph {\n
    center=true;
       node [shape=ellipse margin=0.2 style=rounded fixedsize=false fontname="Zen Maru Gothic"]\n`
       this.materials.forEach(function (material) {
-        dot += `"${material.id}"[label="${material.body}" URL="http://localhost:8000/"]\n`
+        dot += `"${material.id}"[label="${material.body}"]\n`
       })
       this.edges.forEach(function (edge) {
         dot += `"${edge.source}" -> "${edge.target}"\n`
       })
       dot += '}'
-      console.log(dot)
-      graphviz("#graph")
+
+      let graph = await graphviz("#graph")
         .dot(dot)
-        .render();
+        .zoomScaleExtent([0.1, 10]) // ズームの範囲を設定します。必要に応じて調整してください。
+        .zoom(true) // ズームを有効にします。
+        .render()
     },
     finish: function () {
       router.reload({
@@ -65,5 +69,3 @@ export default {
   }
 }
 </script>
-
-<style scoped></style>
